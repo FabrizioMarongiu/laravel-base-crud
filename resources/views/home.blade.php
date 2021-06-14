4230a3ccd3fe9d7160422a37d782f8138ce82947
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-06-14 21:25:43 +0200

    Bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.main')
 
 @section('HomeContent')
-    Home Page
+<div class="bg-info">
+
+    <div class="container text-center">
+        <h1>Visita i nostri Comics</h1>
+    
+        <a class="btn btn-primary" href=" {{ route('comics.index') }} ">Vai al catalogo</a>
+        <img src="https://images8.alphacoders.com/974/thumb-1920-974682.jpg" class="img-fluid" alt="...">
+    
+    </div>
+    
+</div>
+
 @endsection
